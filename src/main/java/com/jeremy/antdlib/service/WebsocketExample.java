e632e9cec7d449868e4d841172964adf5d4068d7
jeremy da <705653685@qq.com>
2020-05-24 19:12:58 +0800

    update websocket

--- src/main/java/com/jeremy/antdlib/service/WebsocketExample.java
+++ src/main/java/com/jeremy/antdlib/service/WebsocketExample.java
@@ -14,21 +14,22 @@ public class WebsocketExample {
 
   public void example(){
 
-    // messageType: 1-上线，2-下线，3-在线名单，4-消息
     // from: 发送方
-    // to: 接收方，all-广播消息
+    // to: 接收方
 
     // 广播发送
     boolean ok = webSocket.sendMessageAll(
-        JSON.toJSONString(ParamUtil.arrayToMap(null, "messageType", 1, "from", "account","to","all")));
+        JSON.toJSONString(ParamUtil.arrayToMap(null, "title", "title", "body", "body", "from", "account","to","all")));
 
     // ok:true-发送成功，false-发送失败
     System.out.println("ok:"+ok);
 
     // 定向发送
     ok = webSocket.sendMessageTo(JSON.toJSONString(
-        ParamUtil.arrayToMap(null, "messageType", 4, "message", "message", "from", "account","to","admin")),
+        ParamUtil.arrayToMap(null, "title", "title", "body", "body", "from", "account","to","admin")),
         "admin");
+
+    System.out.println("ok:"+ok);
 
   }
 }
